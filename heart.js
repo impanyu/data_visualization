e553a8576e5efca0af8ddb369182bd687c1ebe4e
root <root@instance-2.c.impanyu.internal>
2024-01-23 22:14:24 +0000

    change range

--- heart.js
+++ heart.js
@@ -68,7 +68,7 @@ class Particle {
         this.x += dx * expansionRate;
         this.y += dy * expansionRate;
 	this.updateColor();
-        if (this.getDistance()/this.maxDistance < Math.random()*0.1) {
+        if (this.getDistance()/this.maxDistance < Math.random()*0.6) {
             this.reset();
         }
 
